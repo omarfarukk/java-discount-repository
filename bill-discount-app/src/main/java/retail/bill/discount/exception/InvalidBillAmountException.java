e7395adc1cad351f44a40e4b4dc85a5e6ff09a75
khan.omarfaruk@gmail.com <khan.omarfaruk@gmail.com>
2018-12-03 00:21:38 +0400

    Adding comment on exception class

--- bill-discount-app/src/main/java/retail/bill/discount/exception/InvalidBillAmountException.java
+++ bill-discount-app/src/main/java/retail/bill/discount/exception/InvalidBillAmountException.java
@@ -1,10 +1,11 @@
 package retail.bill.discount.exception;
 
+/**
+ * @InvalidBillAmountException class is custom exception and it should be thrown if the bill Amount is Negative
+ * 
+ **/
+
 public class InvalidBillAmountException extends Exception {
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public InvalidBillAmountException(String message) {
